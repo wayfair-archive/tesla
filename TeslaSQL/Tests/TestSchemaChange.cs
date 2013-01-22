492509493c4e1460011760ee197f0c974ac75634
acorwin <acorwin@wayfair.com>
2013-01-22 17:25:25 -0500

    Forgot to commit a test

---
+++ TeslaSQL/Tests/TestSchemaChange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using TeslaSQL;
+using Xunit;
+
+namespace TeslaSQL.Tests {
+    public class TestSchemaChange {
+        [Fact]
+        public void TestEquals() {
+            SchemaChange one = new SchemaChange(1, SchemaChangeType.Add, "sname", "tname", "cname", "ncname");
+            SchemaChange two = new SchemaChange(1, SchemaChangeType.Add, "sname", "tname", "cname", "ncname");
+
+            two.EventType = SchemaChangeType.Drop;
+            Assert.False(one.Equals(two));
+            two.EventType = SchemaChangeType.Add;
+            two.SchemaName = "asdf";
+
+            Assert.False(one.Equals(two));
+            two.SchemaName = "sname";
+
+            Assert.True(one.Equals(two));
+            two.ColumnName = "asdf";
+            Assert.False(one.Equals(two));
+            two.ColumnName = "cname";
+        }
+    }
+}
