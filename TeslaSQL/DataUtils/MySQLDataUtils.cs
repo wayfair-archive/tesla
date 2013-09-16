49225a1a247c6e56625a669130c41f7ef8d0fec7
Clayton Pence <cpence@wayfair.com>
2013-09-16 15:02:31 -0400

    Switching DataType to use longs instead of ints
    In MySQL , there's a field called longtext that has the CHARACTER_MAXIMUM_SIZE
    defined as some huge number that's longer than an Int32 can handle, so we're
    changing the struct and a couple of methods to use Int64s and Nullable<Int64>s
    instead.

--- TeslaSQL/DataUtils/MySQLDataUtils.cs
+++ TeslaSQL/DataUtils/MySQLDataUtils.cs
@@ -1035,7 +1035,6 @@ namespace TeslaSQL.DataUtils {
             var cmd = new MySqlCommand(sql);
             var res = MySqlQuery(dbName, cmd);
             var fields = new Dictionary<TableConf, IList<TColumn>>();
-            var crap = res.Rows[0]["CHARACTER_MAXIMUM_LENGTH"].GetType();
             foreach (DataRow row in res.Rows)
             {
                 var tableName = row.Field<string>("TABLE_NAME");
